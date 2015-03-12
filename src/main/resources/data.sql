dfccbcc1894e438c5ea0e2d1f592570642aecc63
Callaert Anthony <callaertanthony@gmail.com>
2015-03-12 22:34:25 +0100

    create data fixtures on start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO stock (id, quantity) VALUES (1, 10);
+INSERT INTO stock (id, quantity) VALUES (2, 10);
+INSERT INTO stock (id, quantity) VALUES (3, 10);
+INSERT INTO stock (id, quantity) VALUES (4, 10);
+INSERT INTO stock (id, quantity) VALUES (5, 10);
+
+INSERT INTO product (name, price, stock_id) VALUES ('produit_1', 11, 1);
+INSERT INTO product (name, price, stock_id) VALUES ('produit_2', 12, 2);
+INSERT INTO product (name, price, stock_id) VALUES ('produit_3', 13, 3);
+INSERT INTO product (name, price, stock_id) VALUES ('produit_4', 14, 4);
+INSERT INTO product (name, price, stock_id) VALUES ('produit_5', 15, 5);
