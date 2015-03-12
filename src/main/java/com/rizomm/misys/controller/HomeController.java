6b56ef789280162c17410a4463a79bde9257c3d3
GuillaumeVDH <Guillaume.Vanderhaegen@gmail.com>
2015-03-12 12:10:09 +0100

    Redirecting all erros to 404 JSP (i.e errors 401, 404, 500) Created controller for Products using ProductRepository

--- src/main/java/com/rizomm/misys/controller/HomeController.java
+++ src/main/java/com/rizomm/misys/controller/HomeController.java
@@ -10,11 +10,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+
     @RequestMapping(method= RequestMethod.GET)
     public String showProductDetail() {
         return "home";
     }
 
+    /**
+     * This method is called by the Application object when an HTTP "NOT FOUND" error is detected.
+     * It can happens when user ask for an unknown page, so we just redirect him to the 404 error JSP
+     *
+     * @return 404 JSP
+     */
     @RequestMapping("/404")
     public String notFound() {
         return "404";
--- src/main/java/com/rizomm/misys/controller/ProductController.java
+++ src/main/java/com/rizomm/misys/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.rizomm.misys.controller;
 
+import com.rizomm.misys.model.Product;
+import com.rizomm.misys.model.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Guillaume on 3/10/2015.
@@ -10,8 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    private ProductRepository _productRepository;
+
     @RequestMapping("/detail")
     public String showProductDetail() {
         return "product/detail";
     }
+
+
+    @RequestMapping(value="/detail/{id}", method = RequestMethod.GET)
+    public ModelAndView detailProduct(@PathVariable int id){
+        System.out.println("#DEBUG - Asking for product: " + id);
+        Product product = _productRepository.findOne(id);
+        System.out.println(" -id: " + product.getId());
+        System.out.println(" -name: " + product.getName());
+        System.out.println(" -price: " + product.getPrice());
+        return new ModelAndView("product/detail", "product", product);
+    }
+
 }
