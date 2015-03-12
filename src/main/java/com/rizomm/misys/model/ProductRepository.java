6964e5f138d7cf2abeb4fbc86fc21cbdfd6c2dc8
GuillaumeVDH <Guillaume.Vanderhaegen@gmail.com>
2015-03-12 12:00:49 +0100

    Created ProductRepository

---
+++ src/main/java/com/rizomm/misys/model/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.rizomm.misys.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Guillaume on 3/12/2015.
+ */
+
+@Transactional
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+}
