85d5eaab5b10b14968ad94aa7a46d839409dae74
GuillaumeVDH <Guillaume.Vanderhaegen@gmail.com>
2015-03-13 23:30:05 +0100

    Updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,7 @@ INSERT INTO stock (id, quantity) VALUES (5, 10);
 
 INSERT INTO `misys`.`brand` (`id`, `name`) VALUES (NULL, 'Sony'), (NULL, 'Universal');
 
+INSERT INTO `misys`.`product` (`id`, `description`, `name`, `price`, `reference`, `brand_id`, `stock_id`)
+VALUES
+(NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu vestibulum ipsum. Quisque bibendum ut lorem eu dictum.', 'Batman', '999', 'B00JG8GBDM', '1', '2'),
+(NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu vestibulum ipsum. Quisque bibendum ut lorem eu dictum. Nullam sit amet tempor enim, sed lobortis sapien.', 'Batwoman', '99', 'B00JG8GBDS', '2', '5');
