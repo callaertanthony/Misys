cceaf7bec200cc127ae9181ccb76d50d48675f34
Callaert Anthony <callaertanthony@gmail.com>
2015-03-13 16:58:16 +0100

    Revert "Home page main products are now loaded from database"
    This reverts commit 5fe30805b31b8d2e57f39ebc591b47d996c68542.

--- src/main/java/com/rizomm/misys/controller/HomeController.java
+++ src/main/java/com/rizomm/misys/controller/HomeController.java
@@ -1,18 +1,8 @@
 package com.rizomm.misys.controller;
 
-import com.rizomm.misys.model.Product;
-import com.rizomm.misys.model.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 /**
  * Created by Guillaume on 3/12/2015.
@@ -20,31 +10,9 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
-    @Autowired
-    private ProductRepository _productRepository;
-
-
     @RequestMapping(method= RequestMethod.GET)
-    public ModelAndView showProductDetail() {
-        try {
-            Collection<Product> listProducts = new ArrayList<>();
-            Iterable<Product> it = _productRepository.findAll();
-            if(null == it)
-                return new ModelAndView("404");
-            for(Product product : it)
-            {
-                System.out.println("Adding " + product.getName());
-                listProducts.add(product);
-            }
-            if (null == listProducts || listProducts.isEmpty())
-                return new ModelAndView("404");
-            return new ModelAndView("home", "products", listProducts);
-        } catch (IllegalArgumentException e) {
-            System.out.println();
-            return new ModelAndView("404");
-        }
-        //return "home";
+    public String showProductDetail() {
+        return "home";
     }
 
     /**
