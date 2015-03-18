f12a99c825ad800e400403e25ba2e702183d403e
GuillaumeVDH <Guillaume.Vanderhaegen@gmail.com>
2015-03-19 00:09:14 +0100

    Updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,9 @@ INSERT INTO stock (id, quantity) VALUES (3, 10);
 INSERT INTO stock (id, quantity) VALUES (4, 10);
 INSERT INTO stock (id, quantity) VALUES (5, 0);
 
+INSERT INTO `misys`.`user` (`id`, `age`, `email`, `first_name`, `last_name`, `nickname`)
+VALUES ('1', '12', 't@t.fr', 'Charles', 'Hot', 'Charlotte'), ('2', '99', 'z@z.com', 'Mat', 'Ou', 'Matou');
+
 INSERT INTO `misys`.`brand` (`id`, `name`) VALUES (NULL, 'Sony'), (NULL, 'Universal');
 
 INSERT INTO `misys`.`picture_link` (`id`, `link`) VALUES (NULL, 'Penguins.jpg');
@@ -17,3 +20,6 @@ VALUES
   (NULL,
    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu vestibulum ipsum. Quisque bibendum ut lorem eu dictum. Nullam sit amet tempor enim, sed lobortis sapien.',
    'Batwoman', '99', 'B00JG8GBDS', '2', '5', '2');
+
+INSERT INTO `misys`.`review` (`id`, `note`, `review`, `product_id`, `user_id`)
+VALUES (NULL, '6', 'Review 1', '1', '1'), (NULL, '12', 'Review 2', '1', '1');
