95599ab9812c11d16493300f0fef719e513b1786
GuillaumeVDH <Guillaume.Vanderhaegen@gmail.com>
2015-04-13 19:10:48 +0200

    Created services and switched all controllers to them. Removed unused WelcomeController

--- src/main/java/com/rizomm/misys/controller/UserController.java
+++ src/main/java/com/rizomm/misys/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.rizomm.misys.controller;
 
-import com.rizomm.misys.model.UserRepository;
+import com.rizomm.misys.repository.UserRepository;
+import com.rizomm.misys.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -16,11 +16,11 @@ import java.util.Map;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @RequestMapping("/list")
-    public String showUserList(Map<String, Object> model){
-        model.put("list", userRepository.findAll());
+    public String showUserList(Map<String, Object> model) {
+        model.put("list", userService.getAllUsers());
         return "user/list";
     }
 }
--- src/main/java/com/rizomm/misys/repository/ProductRepository.java
+++ src/main/java/com/rizomm/misys/repository/ProductRepository.java
@@ -1,13 +1,16 @@
-package com.rizomm.misys.model;
+package com.rizomm.misys.repository;
 
+import com.rizomm.misys.model.Brand;
+import com.rizomm.misys.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Guillaume on 3/12/2015.
  */
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findFirst10ByBrand(Brand brand);
+    Set<Product> findFirst10ByBrand(Brand brand);
 }
